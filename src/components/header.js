b566ae1b47b2756968abe833e198735fb05c9d19
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-31 11:12:38 -0700

    Add Header component for Albums App

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+// Import Libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make component
+
+const Header = () => {
+  return (
+    <Text>Albums</Text>
+  );
+};
+
+// Make component available to other parts of app
+export default Header;
