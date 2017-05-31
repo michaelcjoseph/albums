31bc0bd857939532c7c8cd578272f66811f28f32
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-31 11:59:13 -0700

    Add AlbumList component with API fetch of data

---
+++ src/components/AlbumList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
