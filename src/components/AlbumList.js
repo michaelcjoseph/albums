1b9c0a0793e3bb973f954d3ac65d68bc5271a1ea
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-05-31 12:14:43 -0700

    Add AlbumDetail component which renders each album from list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      albums: []
+    };
+  }
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log(responseJson);
+      this.setState({
+        albums: responseJson
+      });
     })
     .catch((error) => {
       console.error(error);
     });
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
+    console.log(this.state.albums);
+
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
